8e0445943b037f390e7b0779835b7c3340b9766f
Simon Schrottner <s.schrottner@netconomy.net>
2021-02-07 20:36:45 +0100

    more improvements

--- src/main/groovy/at/schrottner/gradle/GitlabRepositoriesExtension.groovy
+++ src/main/groovy/at/schrottner/gradle/GitlabRepositoriesExtension.groovy
@@ -3,7 +3,6 @@ package at.schrottner.gradle
 import groovy.transform.CompileStatic
 import org.gradle.api.Action
 import org.gradle.api.Project
-import org.gradle.api.artifacts.dsl.ArtifactHandler
 import org.gradle.api.artifacts.dsl.RepositoryHandler
 import org.gradle.api.artifacts.repositories.ArtifactRepository
 import org.gradle.api.artifacts.repositories.AuthenticationContainer
@@ -12,82 +11,99 @@ import org.gradle.api.credentials.HttpHeaderCredentials
 import org.gradle.api.initialization.Settings
 import org.gradle.api.plugins.ExtensionContainer
 import org.gradle.authentication.http.HttpHeaderAuthentication
-
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 @CompileStatic
 public class GitlabRepositoriesExtension {
-
 	public static final String NAME = "gitLab"
 	public static final String REPOSITORY_PREFIX = "GITLAB-"
-	private final Project project
+	public static final String PROPERTY_NAME = "GITLAB"
+	private final Logger logger
 	private final ExtensionContainer extensions
 	private final RepositoryHandler repositories
+	private int afterPosition
 
-	private static def ADDED_REPOSITORIES = [:]
+	String afterRepository = 'MavenLocal'
+	String privateToken
+	boolean applyToProject = true
+	Map<String, Closure<String>> tokens = [:]
+
+	public static final def artifacts = [:]
 
 	GitlabRepositoriesExtension(Settings settings) {
-		settings
-
+		this.logger = LoggerFactory.getLogger(GitlabRepositoriesExtension.class)
+		this.extensions = settings.extensions
+		this.repositories = settings.pluginManagement.repositories
+		setup()
 	}
 
 	GitlabRepositoriesExtension(Project project) {
-		this.project = project
+		this.logger = project.logger
 		this.extensions = project.extensions
 		this.repositories = project.repositories
-
-		project.logger.error("INITIALIZED")
+		setup()
 	}
 
-	ArtifactRepository maven(String id) {
+	void setup() {
+		tokens = [
+				'Job-Token'    : { System.getenv("CI_JOB_TOKEN") },
+				'Deploy-Token' : { System.getenv("GITLAB_DEPLOY_TOKEN") },
+				'Private-Token': { privateToken }
+		]
+		afterPosition = repositories.indexOf(repositories.findByName(afterRepository))
+	}
+
+	void setAfterRepository(String afterRepository) {
+		this.afterRepository = afterRepository
+		afterPosition = repositories.indexOf(repositories.findByName(afterRepository))
+	}
+
+	ArtifactRepository maven(String id, Set<String> tokenSelector = tokens.keySet()) {
 		def repoName = "$REPOSITORY_PREFIX-$id"
 		if (!repositories.findByName(repoName)) {
-			def artifactRepo = generateMavenArtifactRepository(repoName, id)
-			ADDED_REPOSITORIES[repoName] = artifactRepo
-			repositories.maven(artifactRepo)
+
+			def artifactRepo = generateMavenArtifactRepository(
+					repoName,
+					id,
+					tokens.findAll { key, value ->
+						tokenSelector.contains(key)
+					})
+			artifacts[repoName] = artifactRepo
+			def repo = repositories.maven(artifactRepo)
+			// TODO:  rethink this approach do we really need a action? is the removing and readding really a good idea?
+			repositories.remove(repo)
+			repositories.add(++afterPosition, repo)
 		} else {
-			project.logger.info("$repoName already exists")
+			logger.info("GitLab-Repositories: $repoName already exists, i will not reapply it!")
 			repositories.getByName(repoName)
 		}
 	}
 
-	private Action<MavenArtifactRepository> generateMavenArtifactRepository(repoName, id) {
-		new Action<MavenArtifactRepository>() {
-			@Override
-			void execute(MavenArtifactRepository mvn) {
-				mvn.url = "https://gitlab.com/api/v4/groups/$id/-/packages/maven"
-				mvn.name = repoName
-				def credentialName
-				def credentialValue
-				// on GitLab CI
-				if (System.getenv("CI_JOB_TOKEN")) {
-					project.logger.info("$repoName is using CI_JOB_TOKEN")
-					credentialName = 'Job-Token'
-					credentialValue = System.getenv("CI_JOB_TOKEN")
-				}
-				// on jenkins
-				else if (System.getenv("GITLAB_DEPLOY_TOKEN")) {
-					project.logger.info("$repoName is using GITLAB_DEPLOY_TOKEN")
-					// This is actually an Private Token, but deploy keys do not work on group level for now
-					// when gitlab fixes this, we should change this to a Deploy token
-					credentialName = 'Deploy-Token'
-					credentialValue = System.getenv("GITLAB_DEPLOY_TOKEN")
-				}
-				// local development
-				else if (project.hasProperty('gitLabPrivateToken')) {
-					project.logger.info("$repoName is using GitLab Private Token")
-					credentialName = 'Private-Token'
-					credentialValue = project.properties.gitLabPrivateToken
-				}
-				mvn.credentials(HttpHeaderCredentials) {
-					it.name = credentialName
-					it.value = credentialValue
-				}
-				mvn.authentication(new Action<AuthenticationContainer>() {
-					@Override
-					void execute(AuthenticationContainer authentications) {
-						authentications.create('header', HttpHeaderAuthentication)
+	private Action<MavenArtifactRepository> generateMavenArtifactRepository(repoName, id, tokens) {
+		def token = tokens.find { key, value ->
+			value.call()
+		}
+		if (token) {
+			logger.info("GitLab-Repositories: $repoName is using ${token['name']}")
+			new Action<MavenArtifactRepository>() {
+				@Override
+				void execute(MavenArtifactRepository mvn) {
+					mvn.url = "https://gitlab.com/api/v4/groups/$id/-/packages/maven"
+					mvn.name = repoName
+					// on GitLab CI
+
+					mvn.credentials(HttpHeaderCredentials) {
+						it.name = token.key
+						it.value = token.value.call()
 					}
-				})
+					mvn.authentication(new Action<AuthenticationContainer>() {
+						@Override
+						void execute(AuthenticationContainer authentications) {
+							authentications.create('header', HttpHeaderAuthentication)
+						}
+					})
+				}
 			}
 		}
 	}
--- src/main/groovy/at/schrottner/gradle/GitlabRepositoriesSettingsPlugin.groovy
+++ src/main/groovy/at/schrottner/gradle/GitlabRepositoriesSettingsPlugin.groovy
@@ -8,6 +8,14 @@ import org.gradle.api.plugins.ExtensionAware
 public class GitlabRepositoriesSettingsPlugin implements Plugin<Settings>{
 
     public void apply(Settings settings) {
-        new GitlabRepositoriesExtension(settings)
+        GitlabRepositoriesExtension extension = settings.extensions.create(GitlabRepositoriesExtension.NAME, GitlabRepositoriesExtension, settings)
+
+        if( extension.applyToProject) {
+            settings.gradle.beforeProject { project ->
+                GitlabRepositoriesExtension.artifacts.each { key, value ->
+                    project.repositories.maven value
+                }
+            }
+        }
     }
 }
